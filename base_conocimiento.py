60b3d3ebd5c6262bf243a2a8e0f323fcb994d67b
alejo <alejorodasvasquez@gmail.com>
2017-11-13 17:15:00 -0500

    inicio proyecto

---
+++ base_conocimiento.py
@@ -0,0 +1,29 @@
+from pyDatalog import pyDatalog
+pyDatalog.create_terms('hoja, cormo, seudotallo, raquis, racimos, dedo')
+
+
++hoja('seca')
++hoja('quebradiza')
++hoja('no_se_desprende')
++hoja('seca_en_bordes')
++hoja('franja_color_amarilla_intenso')
++cormo('linea_color_marron')
++cormo('linea_color_negro')
++seudotallo('puntos_cafes_oscuros')
++raquis('color_rojizo')
++racimos('deformes')
++dedo('deformes')
+
+
++hoja('amarillamiento_total')
++seudotallo('manchas_acuosas')
++seudotallo('manchas_translucidas_color_amarillento')
++seudotallo('manchas_translucidas_rojizo')
++seudotallo('debil')
+
+
++hoja('amarillamiento')
++hoja('marchita')
++hoja('colo_cafe')
++seudotallo('agriegado')
++seudotallo('agriegado')
---
+++ s_e_platano.py
@@ -0,0 +1,39 @@
+from pyDatalog import pyDatalog, pyEngine
+import logging
+
+pyEngine.Logging = True
+logging.basicConfig(level=logging.INFO)
+
+# def regla_menu(atributo, opcion):
+#   print('Atributo: {} opcion: {}').format(atributo,opcion)
+#   assert_fact( 'tiene_sintoma', 'yes', organo, sintoma )
+
+pyDatalog.create_terms('X, Y, Z, Sintoma, Organo,Enfermedad, print_, twice, tiene_sintoma, diagnostico, enfermedad')
+
++tiene_sintoma('hoja','seca') 
++tiene_sintoma('hoja','quebradiza')
+
+lista_menu = ['seca', 'quebradiza', 'no_se_desprende', 'seca_en_bordes', 'franja_color_amarilla_intenso', 'amarillamiento_total', 'amarillamiento']
+
+diagnostico(Enfermedad, Sintoma) <= tiene_sintoma(Organo, Sintoma) & (Enfermedad == 'moko')
+enfermedad('moko') <= tiene_sintoma('hoja','seca') & tiene_sintoma('hoja','quebradiza')
+#diagnostico('moko') <= tiene_sintoma(Organo, Sintoma1), tiene_sintoma(Organo, Sintoma2) & (Sintoma1 != Sintoma2)
+#diagnostico(Enfermedad, Sintoma1,Sintoma2) <= tiene_sintoma('hoja',Sintoma1) & tiene_sintoma('hoja',Sintoma2) & (Sintoma1 != Sintoma2) & (Enfermedad == 'moko')
+
+def print_(x):
+    print('Print es {}: ').format(type(x))
+
+def mostrar_resultado(resultado):
+  print(resultado)
+
+def pedir_por_pantalla(lista_menu):
+  nombre_sintoma=raw_input('Seleccione: ' + str(lista_menu))
+  return nombre_sintoma
+
+def usar_regla(nombre_sintoma):
+  resultado = diagnostico(Enfermedad,nombre_sintoma)
+  return resultado
+
+nombre_sintoma = pedir_por_pantalla(lista_menu)
+resultado = usar_regla(nombre_sintoma)
+mostrar_resultado(resultado)
