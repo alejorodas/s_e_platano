9b525c6a0870e3fd6de241757607e36da43bc6af
alejo <alejorodasvasquez@gmail.com>
2017-11-14 06:40:54 -0500

    se eliminan metodos y se redefinen en controlador.py

--- s_e_platano.py
+++ s_e_platano.py
@@ -1,43 +1,14 @@
 from pyDatalog import pyDatalog, pyEngine
 from pyDatalog.pyDatalog import assert_fact, load, ask
 import logging
+import controlador
 
 pyEngine.Logging = True
 logging.basicConfig(level=logging.INFO)
 
-pyDatalog.create_terms('Organo,Enfermedad, print_, tiene_sintoma, diagnostico, enfermedad, respuesta')
-pyDatalog.create_terms('X, Y, Z, Sintoma1, Sintoma2')
+pyDatalog.create_terms('diagnostico, enfermedad, respuesta')
 
 lista_menu = ['seca', 'quebradiza', 'no_se_desprende', 'seca_en_bordes', 'franja_color_amarilla_intenso', 'amarillamiento_total', 'amarillamiento']
-
-
-diagnostico('moko') <= respuesta('hoja','seca') & respuesta('hoja','quebradiza')
-
-def almacenar_respuesta_base_dinamica(nombre_sintoma):
-  assert_fact( 'respuesta', 'hoja', nombre_sintoma )
-
-def consultar_base_dinamica():
-  print(ask('respuesta(Organo,Sintoma)'))
-
-def print_(x):
-    print('Print es {}: ').format(type(x))
-
-def mostrar_resultado(resultado):
-  print(resultado)
-
-def pedir_por_pantalla(lista_menu):
-  nombre_sintoma = raw_input('Seleccione: ' + str(lista_menu))
-  return nombre_sintoma
-
-def pantalla(lista_menu):
-  nombre_sintoma=raw_input('Seleccione: ' + str(lista_menu))
-  almacenar_respuesta_base_dinamica(nombre_sintoma)
-
-def usar_regla(nombre_sintoma):
-  return diagnostico(Enfermedad,nombre_sintoma)
-
-def consultar_diagnostico():
-  return diagnostico(Enfermedad)
 
 def main():
   estado = True
@@ -46,12 +17,10 @@ def main():
     if opcion == 'N':
       break
     else:
-      pantalla(lista_menu)
-      # nombre_sintoma = pedir_por_pantalla(lista_menu)
-      # resultado = usar_regla(nombre_sintoma)
-      # mostrar_resultado(resultado)
-  consultar_base_dinamica()
-  resultado = consultar_diagnostico()
-  mostrar_resultado(resultado)
+      controlador.pantalla(lista_menu)
+
+  controlador.consultar_base_dinamica()
+  resultado = controlador.usar_regla_diagnostico()
+  controlador.mostrar_resultado(resultado)
 
 main()
