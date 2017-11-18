d2caf48876380b5643244f002baf0cee0cfda743
alejo <alejorodasvasquez@gmail.com>
2017-11-18 15:01:51 -0500

    se crea prototipo de menu para organos

--- s_e_platano.py
+++ s_e_platano.py
@@ -2,6 +2,7 @@ from pyDatalog import pyDatalog, pyEngine
 from pyDatalog.pyDatalog import assert_fact, load, ask
 import logging
 import controlador
+import ui
 
 pyEngine.Logging = True
 logging.basicConfig(level=logging.INFO)
@@ -13,9 +14,23 @@ lista_menu = ['seca', 'quebradiza', 'no_se_desprende', 'seca_en_bordes', 'franja
 def main():
   estado = True
   while estado:
-    opcion = raw_input('Desea seguir en el programa? [S/N]')
-    if opcion == 'N':
+    ui.menu_principal()
+    opcion = raw_input('Digite su opcion: ')
+    if opcion == '7':
       break
+    elif opcion == '1':
+      ui.menu_hoja()
+      opcion = raw_input('Digite su opcion: ')
+    # elif opcion == '':
+    #   #corno
+    # elif opcion == '':
+    #   #seudotallo
+    # elif opcion == '':
+    #   #raquis
+    # elif opcion == '':
+    #   #racimos
+    # elif opcion == '':
+    #   #dedos
     else:
       controlador.pantalla(lista_menu)
 
