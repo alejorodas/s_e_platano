0780c23c9588424e20df0ca7f6087977fcb312d0
alejo <alejorodasvasquez@gmail.com>
2017-11-18 15:02:16 -0500

    se crea componente de interfaz de usuario

---
+++ ui.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*- 
+def menu_principal():
+
+    print("""
+            Sistema Experto Para El Diagnostico de Enfermedades Platano Dominico Harton
+            ----------------------------------------------------------------------------
+
+                                            Menu Principal
+            ----------------------------------------------------------------------------
+            ----------------------------------------------------------------------------
+            Que organo de la planta ha sido afectado?
+            1. Hoja
+            2. Cormo
+            3. Seudotallo 
+            4. Raquis
+            5. Racimos
+            6. Dedo
+            7. Salir del programa
+    """)
+
+def menu_hoja():
+    print(""" 
+            Seleccione los sintomas que presenta la hoja
+            ----------------------------------------------------------------------------
+            1. Las hojas se secan y se quiebran, pero sin desprenderse de la planta
+            2. Se presenta un secamiento de los bordes de las hojas, seguido de una franja de color amarillo intenso
+            3. Se observa quemazón en el borde de las hojas más viejas que luego avanza a toda la lámina foliar, 
+               ocasionando un amarillamiento total de la hoja.
+            4. Amarillamiento de las hojas más adultas a lo largo del margen foliar que continúa hacia la nervadura central 
+               hasta quedar las hojas completamente marchitas y de color café.
+            5. Retornar al menú principal
+    """)
