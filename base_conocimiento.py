8f9c79db3fc703b8df078d7bc9f2fdb3edba9cea
alejo <alejorodasvasquez@gmail.com>
2017-11-28 09:02:59 -0500

    se crean nuevos predicados para describir las enfermedades. Se amplia regla para Moko

--- base_conocimiento.py
+++ base_conocimiento.py
@@ -17,6 +17,7 @@ pyDatalog.create_terms('Enfermedad')
 +hoja('franja_color_amarilla_intenso')
 +cormo('linea_color_marron')
 +cormo('linea_color_negro')
++cormo('circulo_color_marron')
 +seudotallo('puntos_cafes_oscuros')
 +raquis('color_rojizo')
 +racimos('deformes')
@@ -25,9 +26,8 @@ pyDatalog.create_terms('Enfermedad')
 # Pudricioon acuosa del pseudotallo bacteriosis (Dickeya chrysanthemi)
 +hoja('quemazon_hojas_viejas')
 +hoja('amarillamiento_total')
-+seudotallo('manchas_acuosas')
-+seudotallo('manchas_translucidas_color_amarillento')
-+seudotallo('manchas_translucidas_rojizo')
++seudotallo('manchas_acuosas_translucidas_color_amarillento')
++seudotallo('manchas_acuosas_translucidas_rojizo')
 +seudotallo('debil')
 
 # Mal de Panama
@@ -35,15 +35,42 @@ pyDatalog.create_terms('Enfermedad')
 +hoja('marchita')
 +hoja('colo_cafe')
 +seudotallo('agriegado')
-+seudotallo('agriegado')
++seudotallo('decoloracion_vascular')
++cormo('estrias_necroticas')
++cormo('estrias_oscuras')
++cormo('estrias_azuladas')
 
-
+# Moko
 +tiene_sintoma('hoja','seca') 
 +tiene_sintoma('hoja','quebradiza')
++tiene_sintoma('hoja','no_se_desprende')
++tiene_sintoma('hoja','seca_en_bordes')
++tiene_sintoma('hoja','franja_color_amarilla_intenso')
++tiene_sintoma('cormo','linea_color_marron')
++tiene_sintoma('cormo','linea_color_negro')
++tiene_sintoma('seudotallo','puntos_cafes_oscuros')
 
 
-enfermedad('moko') <= tiene_sintoma('hoja','seca') & tiene_sintoma('hoja','quebradiza')
-diagnostico('moko') <= respuesta('hoja','seca') & respuesta('hoja','quebradiza')
+# Moko o madurabiche 
+enfermedad('moko') <= tiene_sintoma('hoja','seca') & \
+                      tiene_sintoma('hoja','quebradiza') & \
+                      tiene_sintoma('hoja','no_se_desprende') & \
+                      tiene_sintoma('hoja','seca_en_bordes') & \
+                      tiene_sintoma('hoja','franja_color_amarilla_intenso') & \
+                      tiene_sintoma('cormo','linea_color_marron') & \
+                      tiene_sintoma('cormo','linea_color_negro') & \
+                      tiene_sintoma('seudotallo','puntos_cafes_oscuros')
+
+
+
+
+diagnostico('moko') <= respuesta('hoja','seca') & respuesta('hoja','quebradiza') & \
+                      respuesta('hoja','no_se_desprende') & \
+                      respuesta('hoja','seca_en_bordes') & \
+                      respuesta('hoja','franja_color_amarilla_intenso') & \
+                      respuesta('cormo','linea_color_marron') & \
+                      respuesta('cormo','linea_color_negro') & \
+                      respuesta('seudotallo','puntos_cafes_oscuros')
 
 #diagnostico('moko') <= respuesta('hoja',Sintoma1) & respuesta('hoja',Sintoma2)& (Sintoma1 != Sintoma2)
 #diagnostico('moko') <= tiene_sintoma(Organo, Sintoma1), tiene_sintoma(Organo, Sintoma2) & (Sintoma1 != Sintoma2)
@@ -51,3 +78,6 @@ diagnostico('moko') <= respuesta('hoja','seca') & respuesta('hoja','quebradiza')
 
 def consultar_diagnostico():
   return diagnostico(Enfermedad)
+
+def almacenar_respuesta_base_dinamica(organo,nombre_sintoma):
+  assert_fact( 'respuesta', organo, nombre_sintoma )
